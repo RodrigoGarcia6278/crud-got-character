7cc0daebd02f89d71ce0d2e54d69ab781c93a547
RodrigoGarcia6278 <garcia.jacobo.rodrigo@gmail.com>
2022-10-10 06:00:14 -0500

    db fixed1

--- database/db.sql
+++ database/db.sql
@@ -2,7 +2,9 @@ CREATE DATABASE crudgot;
 
 use crudgot;
 
--- name=nombre address=casa phone=edad
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'spyXfam_11';
+flush privileges;
+
 CREATE TABLE customer (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(50) NOT NULL,
