b2890d238f2b6776eb3ac3f1d5b5411d5a29a760
RodrigoGarcia6278 <garcia.jacobo.rodrigo@gmail.com>
2022-10-10 06:07:45 -0500

    db fixed3

--- database/db.sql
+++ database/db.sql
@@ -2,9 +2,6 @@ CREATE DATABASE crudgot;
 
 use crudgot;
 
-ALTER USER 'root'@'containers-us-west-93.railway.app' IDENTIFIED WITH mysql_native_password BY 'B6iGRiGx5oBT6LDGj3wM';
-flush privileges;
-
 CREATE TABLE customer (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(50) NOT NULL,
@@ -15,3 +12,6 @@ CREATE TABLE customer (
 SHOW TABLES;
 
 describe customer;
+
+ALTER USER 'root'@'containers-us-west-93.railway.app' IDENTIFIED WITH mysql_native_password BY 'B6iGRiGx5oBT6LDGj3wM';
+flush privileges;
